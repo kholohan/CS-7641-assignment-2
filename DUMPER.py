59ee7a373cfb79a43df8e37bd04423776e0d6ce1
kyle <kholohan@gmail.com>
2018-03-03 20:40:36 -0500

    - Renamed NN* files to nn_* for better readability - Corrrected labels in nn_* files - Added adult.data - Updated parse.data to handle adult dataset - Added jython standalone

--- DUMPER.py
+++ DUMPER.py
@@ -13,29 +13,25 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
-madelon = pd.read_hdf('datasets.hdf','madelon')        
-madelonX = madelon.drop('Class',1).copy().values
-madelonY = madelon['Class'].copy().values
+adult = pd.read_hdf('datasets.hdf','adult')
+adultX = adult.drop('income',1).copy().values
+adultY = adult['income'].copy().values
 
 
 
 
 
-madelon_trgX, madelon_tstX, madelon_trgY, madelon_tstY = ms.train_test_split(madelonX, madelonY, test_size=0.3, random_state=0,stratify=madelonY)     
+adult_trgX, adult_tstX, adult_trgY, adult_tstY = ms.train_test_split(adultX, adultY, test_size=0.3, random_state=0,stratify=adultY)
 
-pipe = Pipeline([('Scale',StandardScaler()),
-                 ('Cull1',SelectFromModel(RandomForestClassifier(random_state=1),threshold='median')),
-                 ('Cull2',SelectFromModel(RandomForestClassifier(random_state=2),threshold='median')),
-                 ('Cull3',SelectFromModel(RandomForestClassifier(random_state=3),threshold='median')),
-                 ('Cull4',SelectFromModel(RandomForestClassifier(random_state=4),threshold='median')),])
-trgX = pipe.fit_transform(madelon_trgX,madelon_trgY)
-trgY = np.atleast_2d(madelon_trgY).T
-tstX = pipe.transform(madelon_tstX)
-tstY = np.atleast_2d(madelon_tstY).T
+pipe = Pipeline([('Scale',StandardScaler())])
+trgX = pipe.fit_transform(adult_trgX,adult_trgY)
+trgY = np.atleast_2d(adult_trgY).T
+tstX = pipe.transform(adult_tstX)
+tstY = np.atleast_2d(adult_tstY).T
 trgX, valX, trgY, valY = ms.train_test_split(trgX, trgY, test_size=0.2, random_state=1,stratify=trgY)     
 tst = pd.DataFrame(np.hstack((tstX,tstY)))
 trg = pd.DataFrame(np.hstack((trgX,trgY)))
 val = pd.DataFrame(np.hstack((valX,valY)))
-tst.to_csv('m_test.csv',index=False,header=False)
-trg.to_csv('m_trg.csv',index=False,header=False)
-val.to_csv('m_val.csv',index=False,header=False)
+tst.to_csv('a_test.csv',index=False,header=False)
+trg.to_csv('a_trg.csv',index=False,header=False)
+val.to_csv('a_val.csv',index=False,header=False)
